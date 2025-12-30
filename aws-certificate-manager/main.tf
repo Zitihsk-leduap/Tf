236f11da506b0eee3f31b61d6ce3c38221fd5c20
kshitiz <kshitizpaudel5@gmail.com>
2025-12-30 21:36:43 +0545

    fixed certification issue

--- aws-certificate-manager/main.tf
+++ aws-certificate-manager/main.tf
@@ -34,3 +34,11 @@ resource "aws_route53_record" "validation" {
   ttl     = 60
 }
 
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn = aws_acm_certificate.dev_proj_1_acm_arn.arn
+
+  validation_record_fqdns = [
+    for record in aws_route53_record.validation :
+    record.fqdn
+  ]
+}
